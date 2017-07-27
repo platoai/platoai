a42b7801ffd2c7c75ca934498ac28f968f4fad6c
William Myers <will@voxjar.com>
2017-07-27 15:53:41 -0600

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open("README.md") as readme_file:
 
 setuptools.setup(
     name='platoai',
-    version='0.1.3',
+    version='0.2.0',
     description='python implementation of the Plato AI API',
     long_description=readme,
     author='William Myers',
