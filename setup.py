32bc57e75f5f781d751d19bb9f57a05ab55f38c7
William Myers <will@platoai.com>
2017-07-27 17:12:53 -0600

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open("README.md") as readme_file:
 
 setuptools.setup(
     name='platoai',
-    version='0.2.0',
+    version='0.2.2',
     description='python implementation of the Plato AI API',
     long_description=readme,
     author='William Myers',
