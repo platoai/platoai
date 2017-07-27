ce2ab56122f1183de0febb68653de1e25430b3b8
William Myers <will@voxjar.com>
2017-07-27 15:53:18 -0600

    Update and simplify tests

--- test.py
+++ test.py
@@ -2,81 +2,20 @@ from __future__ import print_function
 import os
 import datetime
 import time
-import requests
-import grpc
+import pprint
 import platoai
-from tqdm import tqdm
-
-# TODO: improve testing...
-
-
-def test_local(metadata, file_name='test.wav', channel=None):
-    with open(file_name, 'rb') as f:
-        with tqdm(total=os.path.getsize(file_name)) as pbar:
-            try:
-                pbar.write(
-                    str(
-                        platoai.push(
-                            audio=f,
-                            metadata=metadata,
-                            callbacks=[pbar.update],
-                            channel=channel)))
-            except grpc.RpcError as e:
-                pbar.write(e)
-
-
-def test_remote_simple(metadata,
-                       url='https://students.cs.byu.edu/~wmyers7/test.wav',
-                       channel=None):
-    download_request = requests.get(url, stream=True)
-    r = requests.head(url)  # TODO: shouldnt need a separate head request
-    with tqdm(total=int(r.headers['content-length'])) as pbar:
-        try:
-            pbar.write(
-                str(
-                    platoai.push(
-                        audio=download_request.raw,
-                        metadata=metadata,
-                        callbacks=[pbar.update],
-                        channel=channel)))
-        except grpc.RpcError as e:
-            pbar.write(e)
-
-
-def test_remote_complex(metadata,
-                        url='https://students.cs.byu.edu/~wmyers7/test.wav',
-                        chunk_size=1024,
-                        channel=None):
-    download_request = requests.get(url, stream=True)
-    r = requests.head(url)  # TODO: shouldnt need a separate head request
-    with tqdm(total=int(r.headers['content-length'])) as pbar:
-        with platoai.PushRequest(
-                metadata,
-                channel=channel,
-                chunk_size=chunk_size,
-                callbacks=[pbar.update]) as push_request:
-            for chunk in download_request.iter_content(chunk_size=chunk_size):
-                if chunk:
-                    push_request.write(chunk)
-            try:
-                pbar.write(str(push_request.push()))
-            except grpc.RpcError as e:
-                pbar.write(e)
-
 
 if __name__ == '__main__':
     now = datetime.datetime.now()
 
     metadata = {
         'id':
-            'test_call_id_{}'.format(
-                long(
-                    time.mktime(now.timetuple()) * 1e3 + now.microsecond / 1e3)
-            ),
+            'test_call_id_{}'
+            .format(time.mktime(now.timetuple()) * 1e3 + now.microsecond / 1e3),
         'timestamp':
             now,
-        'callCenter': {
-            'id': '-KjyJwwMAn32WSsaDgbF'
+        'company': {
+            'id': 'b87cf3e2-6820-11e7-8a5d-e7c85d5a5479'
         },
         'agents': [{
             'id': 'test_agent_id',
@@ -92,7 +31,6 @@ if __name__ == '__main__':
             'OUTGOING'
     }
 
-    test_local(metadata)
-    # test_local(metadata, channel=grpc.insecure_channel('0.0.0.0:9001'))
-    # test_remote_simple(metadata, channel=grpc.insecure_channel('0.0.0.0:9001'))
-    # test_remote_complex(metadata, channel=grpc.insecure_channel('0.0.0.0:9001'))
+    url = os.getenv('PLATOAI_API_URL', 'http://localhost:3001/enqueue')
+    with open('test.wav', 'rb') as f:
+        pprint.pprint(platoai.push(metadata, fileobj=f, url=url))
