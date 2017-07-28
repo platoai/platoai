c293a44558fe8b08570bc6fa110cf8c0dea1ad61
William Myers <will@voxjar.com>
2017-07-28 15:23:28 -0600

    Use `identifier` instead of `id` in tests

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import os
 import datetime
 import time
-import pprint
+from pprint import pprint
 import json
 import platoai
 
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     now = datetime.datetime.now()
 
     metadata = {
-        'id':
-            'test_call_id_{}'
+        'identifier':
+            'test_call_identifier_{}'
             .format(time.mktime(now.timetuple()) * 1e3 + now.microsecond / 1e3),
         'timestamp':
             now,
@@ -19,12 +19,12 @@ if __name__ == '__main__':
             'id': 'b87cf3e2-6820-11e7-8a5d-e7c85d5a5479'
         },
         'agents': [{
-            'id': 'test_agent_id',
+            'identifier': 'test_agent_identifier',
             'name': 'test_agent_name',
             'phoneNumber': 1234567890
         }],
         'customers': [{
-            'id': 'test_customer_id',
+            'identifier': 'test_customer_identifier',
             'name': 'test_customer_name',
             'phoneNumber': 9876543210
         }],
@@ -34,4 +34,7 @@ if __name__ == '__main__':
 
     url = os.getenv('PLATOAI_API_URL', 'http://localhost:3001/enqueue')
     with open('test.wav', 'rb') as f:
-        pprint.pprint(json.loads(platoai.push(metadata, fileobj=f, url=url)))
+        r = platoai.push(metadata, fileobj=f, url=url)
+        if not r:
+            print('No response.')
+        pprint(json.loads(r))
