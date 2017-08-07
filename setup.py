dbf47e64136ec64b5d5f9ea2e1681db32be5e495
William Myers <will@platoai.com>
2017-08-07 12:53:37 -0600

    Bump version to 0.3.9

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ with open("README.rst") as readme_file:
 
 setuptools.setup(
     name='platoai',
-    version='0.3.8',
+    version='0.3.9',
     description='python implementation of the Plato AI API',
     long_description=readme,
     author='William Myers',
