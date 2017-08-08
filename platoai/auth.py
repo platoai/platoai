86c4b3a5f8b87afc4c229a674c775d2d0836079b
William Myers <will@voxjar.com>
2017-08-07 20:15:14 -0600

    Improve auth.credentials()

--- platoai/auth.py
+++ platoai/auth.py
@@ -2,12 +2,32 @@ import os
 import json
 
 
-def credentials():
+def credentials(url=None, token=None):
     """Get the credentials configuration from the
     `PLATOAI_APPLICATION_CREDENTIALS` environment variable.
+
+    url can be overriden with the `PLATOAI_API_URL` environment variable.
+    token can be overriden with the `PLATOAI_API_TOKEN` environment variable.
 
     Returns:
         dictionary: The contents of the :ref:`keyfile`.
     """
-    with open(os.environ['PLATOAI_APPLICATION_CREDENTIALS'], 'r') as f:
-        return json.loads(f.read())
+    if not url:
+        url = os.getenv('PLATOAI_API_URL')
+
+    if not token:
+        token = os.getenv('PLATOAI_API_TOKEN')
+
+    if not url or not token:
+        with open(os.environ['PLATOAI_APPLICATION_CREDENTIALS'], 'r') as f:
+            creds = json.loads(f.read())
+
+        if not url:
+            url = creds.get('url', 'https://api.platoai.com:9000')
+
+        if not token:
+            token = creds.get('token')
+
+    assert token and url, 'Must provide token and url'
+
+    return url, token
