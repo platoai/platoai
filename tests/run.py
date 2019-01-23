e225f443a68b5c25bc55ecdec3a162f11c230fa6
William Myers <will@voxjar.com>
2019-01-23 14:28:27 -0700

    Update tests

--- tests/run.py
+++ tests/run.py
@@ -1,41 +1,41 @@
 from __future__ import print_function
+
 import datetime
 from pprint import pprint
-# import json
+
 import voxjar
 
-if __name__ == '__main__':
-    now = datetime.datetime.now()
-
+if __name__ == "__main__":
     metadata = {
-        'identifier': 'test_call_identifier',
-        'timestamp': now,
-        'company': {
-            'id': 'b87cc8ea-6820-11e7-891e-4f389aefc782'
+        "identifier": "test_call_identifier",
+        "timestamp": datetime.datetime.now(),
+        # 'company': {
+        #     'id': 'b87cc8ea-6820-11e7-891e-4f389aefc782'
+        # },
+        "type": {
+            "identifier": "test_call_type_identifier",
+            "name": "test_call_type_name",
         },
-        'type': {
-            'identifier': 'test_call_type_identifier',
-            'name': 'test_call_type_name'
-        },
-        'agents': [{
-            'identifier': 'test_agent_identifier',
-            'name': 'test_agent_name',
-            'phoneNumber': 1234567890
-        }],
-        'customers': [{
-            'identifier': 'test_customer_identifier',
-            'name': 'test_customer_name',
-            'phoneNumber': 9876543210
-        }],
-        'direction': 'OUTGOING',
-        'options': {
-            'processAudio': False
-        },
+        "agents": [
+            {
+                "identifier": "test_agent_identifier",
+                "name": "test_agent_name",
+                "phoneNumber": 1234567890,
+            }
+        ],
+        "customers": [
+            {
+                "identifier": "test_customer_identifier",
+                "name": "test_customer_name",
+                "phoneNumber": 9876543210,
+            }
+        ],
+        "direction": "OUTGOING",
+        "options": {"processAudio": True},
     }
 
-    with open('./test.wav', 'rb') as f:
+    with open("./test.mp4", "rb") as f:
         client = voxjar.Client()
-        print(client)
         try:
             pprint(client.push(metadata, audio=f))
         except RuntimeError as e:
